2d9ff8b25986e3db781b09d82618457fd9920572
youwu123456 <2480508564@qq.com>
2025-01-31 23:39:52 +0800

    Update example.py

--- example.py
+++ example.py
@@ -15,9 +15,9 @@ import logging
 from loseprivacy import LosePrivacy
 
 host = 'https://api.loseprivacy.cyou'
-# 此处填入从LosePrivacy获取的API KEY
+# 'LosePrivacy API KEY'
 apikey = 'LosePrivacy API KEY'
-# 此处填入API KEY的请求速率(速率值受限于API KEY的权限)
+# 2
 ratelimit = 2
 timeout = 60
 
